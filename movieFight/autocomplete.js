d5d3bd3603d218f7b56c0334f302d7f0a33257d2
Bob Grim <grimb300@hotmail.com>
2020-01-31 17:16:17 -0600

    refactor so autocomplete is reusable

---
+++ movieFight/autocomplete.js
@@ -0,0 +1,58 @@
+const createAutoComplete = ({
+  root,
+  renderOption,
+  onOptionSelect,
+  inputValue,
+  fetchData
+}) => {
+  root.innerHTML = `
+    <label><b>Search</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+  `;
+  
+  const input = root.querySelector('input');
+  const dropdown = root.querySelector('.dropdown');
+  const resultsWrapper = root.querySelector('.results');
+  
+  const onInput = async event => {
+    const items = await fetchData(event.target.value);
+    // Clear out old search results
+    resultsWrapper.innerHTML = '';
+  
+    // No results closes the dropdown
+    if (!items.length) {
+      dropdown.classList.remove('is-active');
+      return;
+    }
+  
+    dropdown.classList.add('is-active');
+    for (let item of items) {
+      const option = document.createElement('a');
+  
+      option.classList.add('dropdown-item');
+      option.innerHTML = renderOption(item);
+      option.addEventListener('click', () => {
+        dropdown.classList.remove('is-active');
+        input.value = inputValue(item);
+        onOptionSelect(item);
+      })
+  
+      resultsWrapper.appendChild(option);
+    }
+  };
+  
+  input.addEventListener('input', debounce(onInput, 1000));
+  
+  // Clicking outside the dropdown, closes the dropdown
+  document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+      dropdown.classList.remove('is-active');
+    }
+  });
+  
+};
--- movieFight/index.js
+++ movieFight/index.js
@@ -1,69 +1,30 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: 'ae10b35e',
-      s: searchTerm
-    }
-  });
-
-  // If Error response, return empty array
-  if (response.data.Error) return [];
-  // Else return the Search results
-  return response.data.Search;
-}
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search For a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async event => {
-  const movies = await fetchData(event.target.value);
-  // Clear out old search results
-  resultsWrapper.innerHTML = '';
-
-  // No results closes the dropdown
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-
-  dropdown.classList.add('is-active');
-  for (let movie of movies) {
-    const option = document.createElement('a');
-
-    option.classList.add('dropdown-item');
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-    option.innerHTML = `
+    return `
       <img src="${imgSrc}" />
-      ${movie.Title} 
+      ${movie.Title} (${movie.Year})
     `;
-    option.addEventListener('click', () => {
-      dropdown.classList.remove('is-active');
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    })
-
-    resultsWrapper.appendChild(option);
-  }
-};
-
-input.addEventListener('input', debounce(onInput, 1000));
-
-// Clicking outside the dropdown, closes the dropdown
-document.addEventListener('click', event => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'ae10b35e',
+        s: searchTerm
+      }
+    });
+  
+    // If Error response, return empty array
+    if (response.data.Error) return [];
+    // Else return the Search results
+    return response.data.Search;
   }
 });
 
