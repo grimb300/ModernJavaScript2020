a5cf7c92d080e171e226ddd45df24fa83770907e
Bob Grim <grimb300@hotmail.com>
2020-03-04 11:33:52 -0600

    add edit product routes

--- ecomm/routes/admin/middlewares.js
+++ ecomm/routes/admin/middlewares.js
@@ -3,12 +3,16 @@ const { validationResult } = require('express-validator');
 // This is middleware, so it needs to be written differently?
 // Why isn't this written like the functions in validators.js?
 module.exports = {
-  handleErrors (templateFunc) {
-    return (req, res, next) => {
+  handleErrors (templateFunc, dataCb) {
+    return async (req, res, next) => {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
-        return res.send(templateFunc({ errors }));
+        let data = {};
+        if (dataCb) {
+          data = await dataCb(req);
+        }
+        return res.send(templateFunc({ errors, ...data }));
       }
 
       next();
