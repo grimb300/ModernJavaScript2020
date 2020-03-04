3c3612d69a64a783d937095bca9b23a2be5083d5
Bob Grim <grimb300@hotmail.com>
2020-03-04 09:44:54 -0600

    create middleware for error handling

---
+++ ecomm/routes/admin/middlewares.js
@@ -0,0 +1,17 @@
+const { validationResult } = require('express-validator');
+
+// This is middleware, so it needs to be written differently?
+// Why isn't this written like the functions in validators.js?
+module.exports = {
+  handleErrors (templateFunc) {
+    return (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.send(templateFunc({ errors }));
+      }
+
+      next();
+    };
+  }
+};
